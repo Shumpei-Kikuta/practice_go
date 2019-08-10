1bbaa3918e24c4de5acd84119fc2e34c2e732ba5
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-08-10 14:57:33 +0900

    simple program

---
+++ helloworld/main.go
@@ -0,0 +1,7 @@
+package main // main means executle file
+
+import "fmt" // gain access to the another package
+
+func main() { // executable file must have the main function
+	fmt.Println("Hi there!")
+}
