d627d9c59eabfe0df15583a10dfb06bcf4183322
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-08-12 10:20:04 +0900

    finish cards project

--- cards/deck.go
+++ cards/deck.go
@@ -1,14 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
-// Create a new type of deck
-//  which is a slicce of strings
+type deck []string // define custom type string typeのsubclass 的な
 
-type deck []string //string typeのsubclass 的な
+func newDeck() deck {
+	cards := deck{}
 
-func (d deck) print() {
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() { // receiver
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	//goal ["red", "yellow", "blue"] -> "red, yellow, blue"
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666) // 0666 means anyone can write this file
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil { //error handlingの典型的な書き方
+		// option #1 - log the error and return a call to newDeck()
+		// option #2 - log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,21 @@ package main
 func main() {
 	// var card string = "Ace of Spades" // var val_name type
 	// bool, string, int, float64
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades") // slice is immutable
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards = append(cards, "Six of Spades") // slice is immutable
 
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+
+	// // cards.print()
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
 
