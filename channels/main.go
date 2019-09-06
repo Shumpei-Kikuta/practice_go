71c03dccf60de997289291aca54e85f6fbb99dc6
Shumpei-Kikuta <7484525976@g.ecc.u-tokyo.ac.jp>
2019-09-06 19:18:18 +0900

    tmp

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string) // channel: go routine同士を連携させる(stringの交換)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+	
+	for l := range c{
+		go checkLink(l, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
+		c <- "Might be down I think"
 		fmt.Println(link, "might be down!")
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
